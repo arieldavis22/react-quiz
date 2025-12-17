5dd647e4ee1e0c583e5e7c69abe886dcece742c8
Ariel Davis <amanichai100@gmail.com>
2025-12-17 09:29:14 -0600

    feat: output results

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react";
 
 import QUESTIONS from "../questions.js";
-import completeLogo from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Header() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -25,12 +25,7 @@ export default function Header() {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={completeLogo} alt="Trophy Icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
